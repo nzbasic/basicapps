f54bd28768a8876d439aa37662c1970d5e71aee0
James <jamescoppard024@gmail.com>
2020-01-10 12:06:04 +1300

    Adding comments

--- path.js
+++ path.js
@@ -1,71 +1,63 @@
+// Setting up const for main div
 const $game = $('#game');
+
+// Game data
 var cols = 30;
 var rows = 13;
-var wallKeep = 0;
-var rVars = 0;
+
+// Setting up variables used between functions
+
+// Keeps track of if start and end are placed
 var startCheck = 0;
 var endCheck = 0;
 
+// Keep track of click number 
+var clickNum = 0;
 
+// Used when moving either the start or the end tile
+var moveStart = 0;
+var moveEnd = 0;
+
+// Data for the start and end tiles and a backup of the data
+var rowStart;
+var colStart;
+var rowEnd;
+var colEnd;
+var rowEnd2;
+var colEnd2;
+var colStart2;
+var rowStart2;
+
+// Keeps track of if the mouse is still held down
+var mouseDown;
+
+// Queue for Dijkstra's 
+var queue = new Array(0);
+var tempShift;
+var recurring = 1;
+var pathing = 1;
+
+// Setting up game board by appending rows of divs to the main div, attributes contain the row and col data and weight for later on.
 function gameBoard(rows, cols) {
-    /*var meme = detectmob();
-    if (meme) {
-        const $text = $('<div>').text('Sorry, this does not work on mobile.')
-        $game.append($text);
-    }
-    else { */
-        $game.empty()
-        for (var i = 0; i < rows; i++) {
-            const $row = $('<div>').addClass('row');
-            for (var j = 0; j < cols; j++) {
-                const $col = $('<div>')
-                    .addClass('col')
-                    .attr('data-row', i)
-                    .attr('data-col', j)
-                    .attr('weight', 999);
-                $row.append($col);
-            }
-            $game.append($row);
-        }
-    //}
-}
-
-function detectmob() { 
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ){
-       return true;
-     }
-    else {
-       return false;
-     }
-}
-
-function setup() {
+    $game.empty()
     for (var i = 0; i < rows; i++) {
+        const $row = $('<div>').addClass('row');
         for (var j = 0; j < cols; j++) {
-            var $reset = document.querySelectorAll(`[data-row='${i}'][data-col='${j}']`);
-            $($reset).attr('weight', 999);
-            if ($($reset).hasClass('searched')) {
-                $($reset).removeClass('searched');
-            }
-            if ($($reset).hasClass('path')) {
-                $($reset).removeClass('path');
-            }
-            $($reset).text('');
+            const $col = $('<div>')
+                .addClass('col')
+                .attr('data-row', i)
+                .attr('data-col', j)
+                .attr('weight', 999);
+            $row.append($col);
         }
+        $game.append($row);
     }
 }
 
+// Function that removes all attributes and classes instead of recreating the gameBoard, useful later for other functions.
 function setupReset() {
     queue = new Array(0);
-    sorted = 0;
-    temp = 0;
+    tempShift = 0;
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             var $reset = document.querySelectorAll(`[data-row='${i}'][data-col='${j}']`);
@@ -92,67 +84,54 @@ function setupReset() {
 
 gameBoard(rows,cols);
 
-var clickNum = 0;
-var moveStart = 0;
-var moveEnd = 0;
-var endRemoved = 0;
-var startRemoved = 0;
-var R;
-var C;
-var RE;
-var CE;
-var keepEnd = 0;
-var keepStart = 0;
-var CO;
-var RO;
-var C1;
-var R1;
-var keeper = 0;
-var mouseDown;
-
+// Event listener for mousedown over any tile
 $game.on('mousedown', '.col', function() {
     var $cell = $(this); 
     mouseDown = 1;
-    if (clickNum == 0 || startRemoved == 1) {
+
+    // For the first click, place down the starting tile and increase click number
+    if (clickNum == 0) {
         $cell.addClass('start');
-        R = $cell.data('row');
-        C = $cell.data('col');
-        R1 = R;
-        C1 = C;
+        rowStart = $cell.data('row');
+        colStart = $cell.data('col');
+        rowStart2 = rowStart;
+        colStart2 = colStart;
         clickNum++;        
-        if (startRemoved == 1) {
-            startRemoved = 0;
-        }
     }
-    else if (clickNum == 1 || endRemoved == 1) {
+
+    // For second click, place down the ending tile
+    else if (clickNum == 1) {
         if (!$cell.hasClass('start')) {
             $cell.addClass('end');
-            RE = $cell.data('row');
-            CE = $cell.data('col');
-            RO = RE;
-            CO = CE;
+            rowEnd = $cell.data('row');
+            colEnd = $cell.data('col');
+            rowEnd2 = rowEnd;
+            colEnd2 = colEnd;
             clickNum++;
         }
-        if (endRemoved == 1) {
-            endRemoved = 0;
-        }
     }
+
+    // If the tile being held is start, initiate moving
     else if (clickNum > 0 && $cell.hasClass('start')) {
         moveStart = 1;
         $cell.removeClass('start');
 
-        if (yeet2 == 0){
-            reset(1, keepStart, 1, 1);
+        if (pathing == 0){
+            reset(1, 0, 1, 1);
         }
     }
+
+    // If the tile being held is end, initiate moving
     else if (clickNum > 1 && $cell.hasClass('end')) {
         moveEnd = 1;
         $cell.removeClass('end');
 
-        if (yeet2 == 0){
-            reset(keepEnd, 1, 1, 1);
+        if (pathing == 0){
+            reset(0, 1, 1, 1);
         }
     }
+
+    // If the tile has a wall in it, remove the wall and then recalculate
     else if ($cell.hasClass('wall')) {
         $cell.removeClass('wall');
         checkNodes();
@@ -162,6 +141,8 @@ $game.on('mousedown', '.col', function() {
             endCheck = 0;
         }
     }
+
+    // Place down a wall and recalculate
     else {
         $cell.addClass('wall');
         if ($cell.hasClass('searched')) {
@@ -176,34 +157,39 @@ $game.on('mousedown', '.col', function() {
     }
 })
 
+// Even listener for mouse being released over a tile
 $game.on('mouseup', '.col', function(){
     var $cell = $(this);
     mouseDown = 0;
+
+    // If moveStart was initialised, move the start node to the released location.
     if (moveStart == 1) {
         moveStart = 0;
         if ($cell.hasClass('wall')) {
             $cell.removeClass('wall')
         }
         $cell.addClass('start');
-        R = $cell.data('row');
-        C = $cell.data('col');
-        R1 = R;
-        C1 = C;
+        rowStart = $cell.data('row');
+        colStart = $cell.data('col');
+        rowStart2 = rowStart;
+        colStart2 = colStart;
         if ($cell.hasClass('end')) {
             resetButton();
         }
 
     }
+
+    // If moveEnd was initialised, move the end node to the released location.
     else if (moveEnd == 1) {
         moveEnd = 0;
         if ($cell.hasClass('wall')) {
             $cell.removeClass('wall')
         }
         $cell.addClass('end');
-        RE = $cell.data('row');
-        CE = $cell.data('col');
-        RO = RE;
-        CO = CE;
+        rowEnd = $cell.data('row');
+        colEnd = $cell.data('col');
+        rowEnd2 = rowEnd;
+        colEnd2 = colEnd;
 
         if ($cell.hasClass('start')) {
             resetButton();
@@ -219,6 +205,7 @@ $game.on('mouseup', '.col', function(){
     }
 })
 
+// Event listener for mouse being over a tile, and if mouseDown is active, it places down walls.
 $game.on('mouseover', '.col', function() {
 
     if (mouseDown == 1) {
@@ -232,160 +219,150 @@ $game.on('mouseover', '.col', function() {
     }
 })
 
-var ogDist;
-var newDist;
-var finalDist;
-var queue = new Array(0);
-var sorted;
-var temp;
-var yeet = 1;
-var yeet2 = 1;
 
+// The visualizer function
 function vis() {
-    checkNodes();
-    reset(0,0,1,0);
-    
-    var $start = document.querySelectorAll(`[data-row='${R}'][data-col='${C}']`);
-    var $end = document.querySelectorAll(`[data-row='${RE}'][data-col='${CE}']`);
 
+    // Start by grabbing the start and end data,
+    checkNodes();
+    // Then reset the board, keeping the walls.
+    reset(0,0,1,0);
+
+    // Replace the start and end node
+    var $start = document.querySelectorAll(`[data-row='${rowStart}'][data-col='${colStart}']`);
+    var $end = document.querySelectorAll(`[data-row='${rowEnd}'][data-col='${colEnd}']`);
     $($start).addClass('start');
     $($end).addClass('end');
 
-    yeet2 = 1;
-    yeet = 1;
+    pathing = 1;
+    recurring = 1;
 
-    var $og = document.querySelectorAll(`[data-row='${R}'][data-col='${C}']`);
-    $($og).attr('weight', 0);
+    // For Dijkstra's, start by setting the initial node's weight to 0, all others are infinite (or 999 in this case)
+    $($start).attr('weight', 0);
 
-    recur(R,C);
+    // Then, go to the surrounding tiles and update their weights to 1 and add them to the queue
+    recur(rowStart,colStart);
+    var sortedQueue;
 
-    while(yeet) {
-
-        sorted = queue.sort((a,b) => a[0] - b[0]);
-        temp = sorted.shift();
-        R = temp[1];
-        C = temp[2];
-        recur(R,C);
+    // Then, go through the queue in weight order, calculating all of their surrounding tiles.
+    while(recurring) {
+        sortedQueue = queue.sort((a,b) => a[0] - b[0]);
+        tempShift = sortedQueue.shift();
+        rowStart = tempShift[1];
+        colStart = tempShift[2];
+        recur(rowStart,colStart);
     }
 
-    while(yeet2) {
-        path(RE,CE);
+    while(pathing) {
+        path(rowEnd,colEnd);
     }
 
 }
 
-function clearPath() {
-    for (var i=0; i<rows; i++) {
-        for (var j=0; j<cols; j++){
-            var $clear = document.querySelectorAll(`[data-row='${i}'][data-col='${j}']`);
-            if ($($clear).hasClass('path')) {
-                $($clear).removeClass('path');
-            }
-        }
-    }
-}
-
-function movePath() {
-    yeet2 = 1;
-    while(yeet2) {
-        path(RE,CE);
-    }
-}
-
+// Shows the fastest path from start to end node.
+// It works by starting at the end node, finding its weight and then drawing the path where the next weight is its own minus one.
 function path(R,C) {
-    var $end = document.querySelectorAll(`[data-row='${RE}'][data-col='${CE}']`);
+    var $end = document.querySelectorAll(`[data-row='${rowEnd}'][data-col='${colEnd}']`);
     oldDist = parseInt($($end).attr('weight'));
 
-    if (RE-1 >= 0) {
-        var $temp = document.querySelectorAll(`[data-row='${RE-1}'][data-col='${CE}']`);
+    if (rowEnd-1 >= 0) {
+        var $temp = document.querySelectorAll(`[data-row='${rowEnd-1}'][data-col='${colEnd}']`);
         newDist = parseInt($($temp).attr('weight'));
 
         if (newDist == oldDist - 1){
             if (newDist == 0) {
-                yeet2 = 0;
+                pathing = 0;
                 return;
             }
             $($temp).addClass('path');
-            RE = RE-1;
+            rowEnd = rowEnd-1;
             return;
         }
     }
-    if (RE+1 < rows) {
-        var $temp = document.querySelectorAll(`[data-row='${RE+1}'][data-col='${CE}']`);
+    if (rowEnd+1 < rows) {
+        var $temp = document.querySelectorAll(`[data-row='${rowEnd+1}'][data-col='${colEnd}']`);
         newDist = parseInt($($temp).attr('weight'));
 
         if (newDist == oldDist - 1){
             if (newDist == 0) {
-                yeet2 = 0;
+                pathing = 0;
                 return;
             }
             $($temp).addClass('path');
-            RE = RE+1;
+            rowEnd = rowEnd+1;
             return;
         }
     }
-    if (CE-1 >= 0) {
-        var $temp = document.querySelectorAll(`[data-row='${RE}'][data-col='${CE-1}']`);
+    if (colEnd-1 >= 0) {
+        var $temp = document.querySelectorAll(`[data-row='${rowEnd}'][data-col='${colEnd-1}']`);
         newDist = parseInt($($temp).attr('weight'));
 
         if (newDist == oldDist - 1){
             if (newDist == 0) {
-                yeet2 = 0;
+                pathing = 0;
                 return;
             }
             $($temp).addClass('path');
-            CE = CE-1;
+            colEnd = colEnd-1;
             return;
         }
     }
-    if (CE+1 < cols) {
-        var $temp = document.querySelectorAll(`[data-row='${RE}'][data-col='${CE+1}']`);
+    if (colEnd+1 < cols) {
+        var $temp = document.querySelectorAll(`[data-row='${rowEnd}'][data-col='${colEnd+1}']`);
         newDist = parseInt($($temp).attr('weight'));
 
         if (newDist == oldDist - 1){
             if (newDist == 0) {
-                yeet2 = 0;
+                pathing = 0;
                 return;
             }
             $($temp).addClass('path');
-            CE = CE+1;
+            colEnd = colEnd+1;
             return;
         }
     }
 }
 
+// Finds start and end nodes.
 function checkNodes() {
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             var $check = document.querySelectorAll(`[data-row='${i}'][data-col='${j}']`);
             if ($($check).hasClass('start')) {
                 startCheck = 1;
-                R = i;
-                C = j;
+                rowStart = i;
+                colStart = j;
             }
             if ($($check).hasClass('end')) {
                 endCheck = 1;
-                RE = i;
-                CE = j;
+                rowEnd = i;
+                colEnd = j;
             }
         }
     }
 }
 
-
+// Checks adjacent tiles to see if they can be added to the queue.
 function recur(R, C) {
-
+    var finalDist;
     var $og = document.querySelectorAll(`[data-row='${R}'][data-col='${C}']`);
+
+    // Finds weight of current tile and updates it to searched
     var OG = parseInt($($og).attr('weight'));
     $($og).addClass('searched');
 
+    // Checks if adjacent tile is within the boundary
     if (R-1 >= 0) {
-        
         var $vcell = document.querySelectorAll(`[data-row='${R-1}'][data-col='${C}']`);
+
+        // Makes sure the tile isnt a wall
         if (!$($vcell).hasClass('wall')) {
+
+            // The new weight will be the original weight plus one
             newDist = OG + parseInt(1);
             oldDist = $($vcell).attr('weight');
             
+            // If the new weight is smaller, that will be its new weight.
             if (newDist < oldDist) {
                 finalDist = newDist;   
             }
@@ -394,15 +371,16 @@ function recur(R, C) {
             }
             
             $($vcell).attr('weight', parseInt(finalDist));
-            
             $($vcell).text(parseInt(finalDist));
 
+            // If the tile is the end node, end the recursion 
             if ($($vcell).hasClass('end')) { 
-                yeet = 0;
+                recurring = 0;
                 $($vcell).addClass('searched');
                 return;
             }
 
+            // Add to the queue
             if (!$($vcell).hasClass('searched')) {
                 queue.push([finalDist, R-1, C]);
                 $($vcell).addClass('searched');
@@ -410,6 +388,7 @@ function recur(R, C) {
         }
     }
 
+    // Repeat for each other adjacent tile
     if (R+1 < rows) {
         var $vcell = document.querySelectorAll(`[data-row='${R+1}'][data-col='${C}']`);
         if (!$($vcell).hasClass('wall')) {
@@ -427,7 +406,7 @@ function recur(R, C) {
             $($vcell).text(parseInt(finalDist));
 
             if ($($vcell).hasClass('end')) { 
-                yeet = 0;
+                recurring = 0;
                 $($vcell).addClass('searched');
                 return;
             }
@@ -456,7 +435,7 @@ function recur(R, C) {
             $($vcell).text(parseInt(finalDist));
 
             if ($($vcell).hasClass('end')) { 
-                yeet = 0;
+                recurring = 0;
                 $($vcell).addClass('searched');
                 return;
             }
@@ -485,7 +464,7 @@ function recur(R, C) {
             $($vcell).text(parseInt(finalDist));
 
             if ($($vcell).hasClass('end')) { 
-                yeet = 0;
+                recurring = 0;
                 $($vcell).addClass('searched');
                 return;
             }
@@ -498,10 +477,9 @@ function recur(R, C) {
     }
 }
 
-var wallArray = new Array(0);
-var temp;
-
+// Stores the wall data for when the board is reset to keep the walls.
 function wallKeeper() {
+    var wallArray = new Array(0);
     for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols; j++) {
             var $wallCheck = document.querySelectorAll(`[data-row='${i}'][data-col='${j}']`);
@@ -513,29 +491,25 @@ function wallKeeper() {
     setupReset();
     var len = wallArray.length;
     for (var k = 0; k < len; k++) {
-        temp = wallArray.shift();
-        var $wallPlace = document.querySelectorAll(`[data-row='${temp[0]}'][data-col='${temp[1]}']`);
+        tempShift = wallArray.shift();
+        var $wallPlace = document.querySelectorAll(`[data-row='${tempShift[0]}'][data-col='${tempShift[1]}']`);
         $($wallPlace).addClass('wall');
     }
 }
 
 
-
+// Some reset functions
 function reset(keepEnd, keepStart, wallKeep, rVars) {
-
     if (rVars) {
         clickNum = 0;
         moveStart = 0;
         moveEnd = 0;
-        endRemoved = 0;
-        startRemoved = 0;
-        R = 0;
-        C = 0;
-        yeet = 1;
-        yeet2 = 1;
+        rowStart = 0;
+        colStart = 0;
+        recurring = 1;
+        pathing = 1;
         queue = new Array(0);
-        sorted = 0;
-        temp = 0;
+        tempShift = 0;
     }
 
     if (wallKeep) {
@@ -546,25 +520,23 @@ function reset(keepEnd, keepStart, wallKeep, rVars) {
     }
 
     if (keepEnd == 1) {
-        var $end = document.querySelectorAll(`[data-row='${RO}'][data-col='${CO}']`);
+        var $end = document.querySelectorAll(`[data-row='${rowEnd2}'][data-col='${colEnd2}']`);
         $($end).addClass('end');
         moveStart = 1;
         clickNum = 2;
-        RE = RO;
-        CE = CO;
+        rowEnd = rowEnd2;
+        colEnd = colEnd2;
         keepEnd = 0;
-        keeper = 1;
     }
 
     if (keepStart == 1) {
-        var $start = document.querySelectorAll(`[data-row='${R1}'][data-col='${C1}']`);
+        var $start = document.querySelectorAll(`[data-row='${rowStart2}'][data-col='${colStart2}']`);
         $($start).addClass('start');
         moveEnd = 1;
         clickNum = 2;
-        R = R1;
-        C = C1;
+        rowStart = rowStart2;
+        colStart = colStart2;
         keepStart = 0;
-        keeper = 1;
     }
 }
 
@@ -572,15 +544,12 @@ function resetButton() {
     clickNum = 0;
     moveStart = 0;
     moveEnd = 0;
-    endRemoved = 0;
-    startRemoved = 0;
-    R = 0;
-    C = 0;
-    yeet = 1;
-    yeet2 = 1;
+    rowStart = 0;
+    colStart = 0;
+    recurring = 1;
+    pathing = 1;
     queue = new Array(0);
-    sorted = 0;
-    temp = 0;
+    tempShift = 0;
     endCheck = 0;
     startCheck = 0;
     gameBoard(rows,cols);
--- reaction.js
+++ reaction.js
@@ -1,11 +1,8 @@
-const $game = $('#game');
-var timeTotal = 0;
-var numClick = 0;
-var first = 0;
-var timeLast;
-var timeAverage = 0;
-var timePrev;
 
+// Setup const for div 
+const $game = $('#game');
+
+// Setup board by appending rows of divs to the origin div
 function gameBoard(rows, cols) {
     $game.empty()
     for (var i = 0; i < rows; i++) {
@@ -23,25 +20,37 @@ function gameBoard(rows, cols) {
 
 gameBoard(10,15);
 
+// A new div to show average time
 const $ave = $('<div>').addClass('average');
 $game.append($ave);
+var timeAverage = 0;
 $ave.text('Average:' + timeAverage + 'ms');
 
+// Random coordinates for first object
 randX = Math.floor(Math.random() * 9);
 randY = Math.floor(Math.random() * 14);
 var $randCell = document.querySelectorAll(`[data-row='${randX}'][data-col='${randY}']`);
 $($randCell).addClass('circle')
 
+// Event listener for clicking a circle
 $game.on('click', '.col.circle', function() {
+    var timePrev;
+    var timeLast;
+    var timeTotal = 0;
+    var numClick = 0;
+    var first = 1;
+
     var $cell = $(this);
     $cell.removeClass('circle');
     
-    if (!first) {
+    // If it is the first circle, setup timing
+    if (first) {
         timeNow = new Date().getTime();
         timeLast = 0;
         timeAverage = 0;
-        first = 1;
+        first = 0;
     } else {
+        // Increase number of clicks, grab new timings, calculate average
         numClick++;
         timePrev = timeNow;
         timeNow = new Date().getTime();
@@ -50,6 +59,7 @@ $game.on('click', '.col.circle', function() {
         timeAverage = timeTotal / numClick;
     }
 
+    // If left idle, stop time from extending past div 
     if (timeLast > 99999) {
         timeLast = 'a lot';
     }
@@ -57,16 +67,20 @@ $game.on('click', '.col.circle', function() {
         timeLast = '';
     }
     
+    // Update the average time on the display div
     $ave.text('Average:' + parseInt(timeAverage) + 'ms');
 
+    // Calculate new random coords
     randX = Math.floor(Math.random() * 9);
     randY = Math.floor(Math.random() * 14);
     var $randCell = document.querySelectorAll(`[data-row='${randX}'][data-col='${randY}']`);
     $($randCell).addClass('circle')
+
+    // Show the time taken for the last click on the next circle.
     $($randCell).text(timeLast);
 })
 
-
+// Reset the board.
 function reset() {
     gameBoard(10,15);
     timeAverage = 0;
